0e7dcc669dc94c1eb8b59be6e1f40459f962515e
Vyacheslav <v.guber2000@gmail.com>
2022-05-27 23:00:23 +0300

    Lesson-8

--- app/src/main/java/com/vgubert/notesappmvvm/database/firebase/AppFirebaseRepository.kt
+++ app/src/main/java/com/vgubert/notesappmvvm/database/firebase/AppFirebaseRepository.kt
@@ -1,20 +1,38 @@
 package com.vgubert.notesappmvvm.database.firebase
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.vgubert.notesappmvvm.database.DatabaseRepository
 import com.vgubert.notesappmvvm.model.Note
+import com.vgubert.notesappmvvm.utils.Constants.Keys.SUBTITLE
+import com.vgubert.notesappmvvm.utils.Constants.Keys.TITLE
+import com.vgubert.notesappmvvm.utils.FIREBASE_ID
 import com.vgubert.notesappmvvm.utils.LOGIN
 import com.vgubert.notesappmvvm.utils.PASSWORD
 
 class AppFirebaseRepository : DatabaseRepository {
 
     private val mAuth = FirebaseAuth.getInstance()
-    override val readAll: LiveData<List<Note>>
-        get() = TODO("Not yet implemented")
+    private val  database = Firebase.database.reference
+        .child(mAuth.currentUser?.uid.toString())
+
+    override val readAll: LiveData<List<Note>> = AllNotesLiveData()
 
     override suspend fun create(note: Note, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        val noteId = database.push().key.toString()
+        val mapNotes = hashMapOf<String, Any>()
+
+        mapNotes[FIREBASE_ID] = noteId
+        mapNotes[TITLE] = note.title
+        mapNotes[SUBTITLE] = note.subtitle
+
+        database.child(noteId)
+            .updateChildren(mapNotes)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { Log.d("checkData", "Failed to add new note") }
     }
 
     override suspend fun update(note: Note, onSuccess: () -> Unit) {
